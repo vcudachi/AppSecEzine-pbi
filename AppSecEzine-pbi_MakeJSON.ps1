9e447c6b3a40721ec97fccb2619682ee518aadc3
vcudachi <vcudachi@gmail.com>
2024-09-11 19:58:12 +0300

    Initial commit

---
+++ AppSecEzine-pbi_MakeJSON.ps1
@@ -0,0 +1,113 @@
+﻿#requires -version 7.4
+<#
+.SYNOPSIS
+  Makes JSON file from AppSecEzine
+.DESCRIPTION
+  This script makes a JSON file that is used to build a PowerBI report for AppSecEzine. Original content location: https://github.com/Simpsonpt/AppSecEzine
+.INPUTS
+  Reads AppSecEzine from .\AppSecEzine
+.OUTPUTS
+  JSON file stored in .\AppSecEzine-pbi.json
+.NOTES
+  Version:        1.0
+  Author:         vcudachi
+  Creation Date:  2024-0909@1900
+  License:        Creative Commons Attribution NonCommercial ShareAlike (CC-NC-SA: https://creativecommons.org/licenses/by-nc-sa/3.0/)
+  GitHub:         https://github.com/vcudachi/AppSecEzine-pbi
+  
+.EXAMPLE
+  #Powershell:
+  git clone https://github.com/vcudachi/AppSecEzine-pbi.git
+  cd AppSecEzine-pbi
+  git clone 'https://github.com/Simpsonpt/AppSecEzine.git'
+  . .\AppSecEzine-pbi_MakeJSON.ps1
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+If (!(Test-Path .\AppSecEzine -PathType Container -ErrorAction SilentlyContinue)) {
+    Write-Host 'AppSecEzine is not found. Read README.md for details.' -ForegroundColor RED
+    Start-Sleep -s 5
+}
+Else {
+    $Ezines = Get-ChildItem -LiteralPath .\AppSecEzine\Ezines -File -ErrorAction SilentlyContinue | Sort-Object { Try { [int]$_.Name.Substring(0, $_.Name.IndexOf('-')) }Catch {}; }
+    $EzineObjects = [System.Collections.Generic.List[PSCustomObject]]::New()
+    
+    #Extract metadata
+    $Pattern = '^#+\s+Week:\s+(?<Week>\d+)\s+\|\s+Month:\s+(?<Month>\w+)\s+\|\s+Year:\s+(?<Year>\d+)\s+\|\s+Release\s+Date:\s+(?<ReleaseDate>\S+)\s+\|\s+Edition:\s+#*(?<Edition>\d+)º*\s+#+$'
+    ForEach ($Ezine in $Ezines) {
+        $EzineContent = Get-Content $Ezine.FullName -Encoding UTF8
+        If ($EzineContent[6] -match $Pattern) {
+            $Week = [int]$matches.Week
+            $Month = $matches.Month
+            $Year = [int]$matches.Year
+            $ReleaseDate = $matches.ReleaseDate
+            $Edition = [int]$matches.Edition
+        }
+        Else {
+            #This script needs to be adjusted
+            Write-Error -Message "Ezine content not match pattern. Skipping '$($Ezine.Name)'"
+            Continue
+        }
+        $Section = $null
+
+        #Parsing
+        $SmallPattern = '^(?<Prop>\w+):\s+(?<Val>.+)$'
+        $EzineContent | ForEach-Object {
+            Switch ($_) {
+                '''  ╔╦╗┬ ┬┌─┐┌┬┐  ╔═╗┌─┐┌─┐' { $Section = 'MustSee' }
+                '''  ╦ ╦┌─┐┌─┐┬┌─' { $Section = 'Hack' }
+                '''  ╔═╗┌─┐┌─┐┬ ┬┬─┐┬┌┬┐┬ ┬' { $Section = 'Security' }
+                '''  ╔═╗┬ ┬┌┐┌' { $Section = 'Fun' }
+            }
+            $uri = $null
+            If ($Section) {
+                If ($_ -eq [String]::Empty) {
+                    If ($Flag) {
+                        $EzineObjects.Add($EzineObject)
+                    }
+                    $EzineObject = [PSCustomObject]@{
+                        Week        = $Week
+                        Month       = $Month
+                        Year        = $Year
+                        ReleaseDate = $ReleaseDate
+                        Edition     = $Edition
+                        Section     = $Section
+                    }
+                    $Flag = 0
+                }
+                ElseIf ($_ -match $SmallPattern) {
+                    If ($matches.Prop -in 'URL', 'Description') {
+                        $Prop = $matches.Prop
+                        $Val = $matches.Val
+                        If ($Prop -eq 'URL') {
+                            $Val = $Val -replace '\s.+$', ''  #repair bad URLs
+                            Try { $uri = [uri]$Val } Catch { $uri = $null }
+                        }
+                    }
+                    Else {
+                        $Prop = 'Other'
+                        $Val = $matches.Val
+                    }
+                    #The following is not good, but it works
+                    If ($Prop -in $EzineObject.psobject.Members.Name) {
+                        $EzineObject.$Prop = $EzineObject.$Prop, $Val -join "`n"
+                        If ($uri) {
+                            $EzineObject.WebSite = $EzineObject.WebSite, $uri.Host -join "`n"
+                        }
+                    }
+                    Else {
+                        $EzineObject | Add-Member -MemberType NoteProperty -Name $Prop -Value $Val
+                        If ($uri) {
+                            $EzineObject | Add-Member -MemberType NoteProperty -Name 'WebSite' -Value $uri.Host
+                        }
+                    }
+                    $Flag = 1
+                }
+            }
+        }
+    }
+}
+
+#Output JSON file
+$EzineObjects | ConvertTo-Json | Set-Content -LiteralPath .\AppSecEzine-pbi.json -Force -Encoding UTF8
---
+++ README.md
@@ -0,0 +1,39 @@
+﻿# AppSec Ezine PowerBI Report
+Original content is located here: https://github.com/Simpsonpt/AppSecEzine
+
+## Contents
+1. **AppSecEzine-pbi.pbix** - PowerBI report.
+2. **AppSecEzine-pbi.json** - Structured data in JSON format.
+3. **AppSecEzine-pbi_MakeJSON.ps1** - Simple parser is used to make JSON file.
+
+## Requirements
+- Windows
+- git
+- Powershell 7.4
+- PowerBI Desktop Application
+
+## The reason and refresh instructions
+The reason i made this Power BI report for AppSec Ezine is to be able to search for topics that interest me in a more structured manner. PBIX file contais AppSecEzines from the beginning to the date of PBIX file upload. 
+In case you want to update the contents of the file, then you need to:
+1. Simply using **git** clone the **AppSecEzine** folder next to **AppSecEzine-pbi_MakeJSON.ps1**
+2. Run my simple parser for the data: **AppSecEzine-pbi_MakeJSON.ps1** to update the JSON file.
+3. The next step is to correctly pass JSON file into the PowerBI report and refresh it. Open report in PowerBI Desktop application and use **"Transform data\Advanced Editor"** to change the location of the JSON file (Replace ***Source = Json.Document(File.Contents("C:\Temp\AppSecEzine-pbi\AppSecEzine-pbi.json")),*** with correct path).
+4. Refresh the report
+
+```Powershell
+git clone https://github.com/vcudachi/AppSecEzine-pbi.git
+cd AppSecEzine-pbi
+git clone 'https://github.com/Simpsonpt/AppSecEzine.git'
+. .\AppSecEzine-pbi_MakeJSON.ps1
+```
+
+
+**Screenshot**
+![screenshot](ReportScreenshot.png)
+
+
+**License:**
+
+This project is under Creative Commons Attribution NonCommercial ShareAlike ([CC-NC-SA](https://creativecommons.org/licenses/by-nc-sa/3.0/ "CC-NC-SA")).
+
+![CC-NC-SA](http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png "License")
